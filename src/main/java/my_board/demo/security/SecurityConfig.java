61d7aa6136057889b01f4cfedafeef9dc9cc6df8
김재범 <kimjb@kimjb-ui-MacBookAir.local>
2025-12-24 01:01:37 +0900

    로그인 페이지 수정

--- src/main/java/my_board/demo/security/SecurityConfig.java
+++ src/main/java/my_board/demo/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 // 3. 요청별 권한 설정
                 .authorizeHttpRequests(auth -> auth
                         // 로그인, 회원가입, 메인 페이지, 정적 리소스는 누구나 접근 가능
-                        .requestMatchers("/", "/members/login", "members/join", "/css/**", "/images/**", "/js/**", "/favicon.ico").permitAll()
+                        .requestMatchers("/", "/login", "members/join", "/css/**", "/images/**", "/js/**", "/favicon.ico").permitAll()
                         // 게시글 목록 보기(GET /posts)도 누구나 접근 가능 (상세 보기도 포함)
                         .requestMatchers("posts/**").permitAll()
                         // 그 외 요청(글 쓰기, 수정 등)은 인증된 사용자만 가능
